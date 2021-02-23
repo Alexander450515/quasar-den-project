8daa4b3a091697cb45319fceddac1d2efef93738
Alexander450515 <62418411+Alexander450515@users.noreply.github.com>
2021-02-23 14:30:13 +0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-layout view="hHh Lpr lff">
+    <q-header elevated class="bg-black">
+      <q-toolbar>
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        <q-toolbar-title>Header</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      mini-to-overlay
+      :width="200"
+      :breakpoint="500"
+      bordered
+      content-class="bg-grey-3"
+    >
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="inbox" />
+            </q-item-section>
+
+            <q-item-section>
+              Inbox
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+// import EssentialLink from "components/EssentialLink.vue";
+
+const linksData = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev"
+  }
+];
+
+export default {
+  name: "MainLayout",
+  components: {
+    //  EssentialLink
+  },
+  data() {
+    return {
+      drawer: false,
+      miniState: true
+    };
+  }
+};
+</script>
